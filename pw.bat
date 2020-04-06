2a4b5260b1aa1aa04af06eddc7376d6200fd6602
Petr Kaminskiy <32541031+KaminskiyIT@users.noreply.github.com>
2020-04-06 21:36:32 +0300

    Add files via upload

---
+++ pw.bat
@@ -0,0 +1,2 @@
+@py.exe C:\py_lessons\automatic_by_py\6\project1\pw.py %*
+@pause
---
+++ pw.py
@@ -0,0 +1,21 @@
+#! python3
+# pw.py - Программа для незащищенного копирования паролей.
+
+PASSWORDS = {'email': 'F7mbzdjkbmzx88gjhsjJHGYbn7',
+			 'blog': 'KHkbjkjhIUhJHKjnjBIUhkjnbj',
+			 'luggage': '12345'}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+	print('Использование: python pw.py [имя учетной записи] - копирование пароля учетной записи')
+	sys.exit()
+
+account = sys.argv[1] # первый аргумент командной строки - это
+					  # имя учетной записи
+					  
+if account in PASSWORDS:
+	pyperclip.copy(PASSWORDS[account])
+	print('Пароль для ' + account + ' скопирован буфер.')
+else:
+	print('Учетная запись ' + account + ' отсутствует в списке.')
+	
